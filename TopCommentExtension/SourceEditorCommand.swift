c826d81597d2579eca01c31075d63853cd11f35d
Nathan Johnson <nathan_johnson@byu.edu>
2017-01-06 10:45:19 -0700

    Renamed command identifier ChangeDate to UpdateCreationDate

--- TopCommentExtension/SourceEditorCommand.swift
+++ TopCommentExtension/SourceEditorCommand.swift
@@ -6,17 +6,16 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     
     public func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void) {
         switch invocation.commandIdentifier {
-        
-        case "com.alejandromp.XcodeTopComment.TopCommentExtension.RemoveTopComment":
-            removeTopComment(from: invocation.buffer.lines)
-        case "com.alejandromp.XcodeTopComment.TopCommentExtension.AddLastModifiedBy":
-            addLastModifiedBy(in: invocation.buffer.lines)
-        case "com.alejandromp.XcodeTopComment.TopCommentExtension.UpdateDateModified":
-            updateDate(in: invocation.buffer.lines, with: .lastModifiedBy)
-        case "com.alejandromp.XcodeTopComment.TopCommentExtension.ChangeDate":
-            updateDate(in: invocation.buffer.lines, with: .createdBy)
-        default:
-            fatalError("\(invocation.commandIdentifier) not supported.")
+            case "com.alejandromp.XcodeTopComment.TopCommentExtension.RemoveTopComment":
+                removeTopComment(from: invocation.buffer.lines)
+            case "com.alejandromp.XcodeTopComment.TopCommentExtension.AddLastModifiedBy":
+                addLastModifiedBy(in: invocation.buffer.lines)
+            case "com.alejandromp.XcodeTopComment.TopCommentExtension.UpdateDateModified":
+                updateDate(in: invocation.buffer.lines, with: .lastModifiedBy)
+            case "com.alejandromp.XcodeTopComment.TopCommentExtension.UpdateCreationDate":
+                updateDate(in: invocation.buffer.lines, with: .createdBy)
+            default:
+                fatalError("\(invocation.commandIdentifier) not supported.")
         }
         
         completionHandler(nil)
